a45f8cc36f3fc2efff81345264f48c66fe9544d3
combats222a <combats222a@gmail.com>
2026-08-22 08:46:17 +0300

    Delete backend directory

--- backend/src/controllers/anomaly.controller.ts
+++
@@ -1,7 +0,0 @@
-import * as service from '../services/anomaly.service';
-import { wrap } from './asyncHandler';
-
-export default {
-  get: wrap(service.getAnomaly),
-  set: wrap(service.setAnomaly),
-};
